38a59372e3e57971c91c82c74970010482d406bb
AnderGI <agibarguen@gmail.com>
2024-04-14 09:24:13 +0200

    Configuration problems resolved

--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,12 @@
 #! /usr/bin/env node
 
-import { input, select, expand } from "@inquirer/prompts";
-import { USER_CHOICES } from "./domain/user-choices.js";
-import { addUserChoices } from "./domain/add-user-choice.js";
+import { program } from 'commander';
 
 (async () => {
-  const name:string = await input({ message: "Enter your name: " });
-  const option:number = await select({
-    message: `Hi ${name} what would you like to do ?`,
-    choices: addUserChoices(USER_CHOICES),
-  });
+  program
+    .option('--first')
+    .option('-s, --separator <char>');
+  
+  program.parse();
 
-  // ocp
-  const handleOption = (option:number) => {
-    if(option === 1){
-      // email
-      console.log("Send me an email to agibarguen@gmail.com");
-    }
-  }
-
-  handleOption(option);
-
-  const answer = await expand({
-    message: 'Conflict on file.js',
-    default: 'y',
-    choices: [
-      {
-        key: 'y',
-        name: 'Overwrite',
-        value: 'overwrite',
-      },
-      {
-        key: 'a',
-        name: 'Overwrite this one and all next',
-        value: 'overwrite_all',
-      },
-      {
-        key: 'd',
-        name: 'Show diff',
-        value: 'diff',
-      },
-      {
-        key: 'x',
-        name: 'Abort',
-        value: 'abort',
-      },
-    ],
-  });
-
-  console.log(answer);
 })();
